393cf6f10d41739e5594d578e6d83481d41c9b5e
Exaiystic <36638173+Exaiystic@users.noreply.github.com>
2019-05-20 15:23:04 +0100

    Add files via upload

--- assignment.java
+++ assignment.java
@@ -5,22 +5,32 @@ import java.util.*;
 
 public class assignment {
 
-	public static void main(String[] args) {
+	//Main Method
+    ////////////////////////////////////////////////////////////////////
+	//The main method is used to call other functions
+	public static void main(String[] args) { //The main method is called be default
 		//User Login
-		Login admin = new Login(); //Create a new login called admin.
-		admin.getLoginCredentials("admin", "password123"); //We state the credentials in the order of username then password
+		//First line creates a login object called admin so that we can call the function
+		//The second line then calls the object's function with the credentials in order username and password
+		Login admin = new Login(); 
+		admin.getLoginCredentials("admin", "password123");
 		
-		shipSearch();
+		//Ship Search
+		shipSearch(); //Calls the shipSearch method. It is a static method in this class so it is called like this
 		
-		retryPrompt();
+		retryPrompt(); //Calls the retryPrompt method. It is a static method in this class so it is called like this
 	}
 	
+	//Ship Search Method
+	////////////////////////////////////////////////////////////////////
+	//The shipSearch function is where an inputted ship is searched for and displayed
 	public static void shipSearch() {
 		// TODO Auto-generated method stub
 		
 		ArrayList<Ship_Superclass> ships = new ArrayList<Ship_Superclass>(); //Array for ship scan - needs to include everything
 		
 		//  FOR TESTSHIP 1
+		//Creating all components for Test Ship
 		Engine Speeder_Mk1 = new Voyager_Engine("Speeder Mk1", "Hydrogen", 5.0, 10.0, 50.0); 
 		Radar Spy_Mk1 = new Radar("Spy Mk1", 100.0, 5.0, 20);
 		Hull Kalvers_50mm = new Hull("Kalvers 50mm", "Titanium Alloy", 250.0, 50, 450.0, 5.5);
@@ -36,29 +46,33 @@ public class assignment {
 		System.out.println(ships);
 		
 		System.out.println("Input the name of the ship:"); //Prompting user for ship name
-		Scanner shipScan = new Scanner(System.in); //Creating a place for the input to be stores
+		Scanner shipScan = new Scanner(System.in); //Creating a place for the input to be stored
 		String shipSearch = shipScan.nextLine(); //Getting the input and storing it
 		
 		for (Ship_Superclass s: ships) { //For loop saying "For as long as there are ships in Ship_Superclass
 			if (s.getName().equals(shipSearch)) { //If s is equal to searched ship then do the below:
 				System.out.println(s.display()); //Display the stats of the ship s
-			    System.out.println("Expand? Y/N");
-			    Scanner expandScanner = new Scanner(System.in);
-			    String expandResponse = expandScanner.nextLine();
-			    if (expandResponse.equals("Y") ) {
-			        System.out.println(s.displayExpand());
+			    System.out.println("Expand? Y/N"); //Prompts the user if they want to expand the info
+			    Scanner expandScanner = new Scanner(System.in); //Creating a place for the input to be stored
+			    String expandResponse = expandScanner.nextLine(); //Getting the input and storing it
+			    if (expandResponse.equals("Y") ) { //If the variable equals "Y"...
+			        System.out.println(s.displayExpand()); //Run the displayExpand method in whatever s is
 			    }
-			} else {
-				System.out.println("NO SHIP DETECTED");
+			} else { //Otherwise...
+				System.out.println("NO SHIP DETECTED"); //Say "No Ship Detected"
 			}
 		}
 	}
 	
+	//Retry Prompt Method
+    ////////////////////////////////////////////////////////////////////
+	//This method asks the user if they want to find another ship, it also 
+	//stores the user's input
 	public static void retryPrompt() {
 		System.out.println("Scan for another ship? Y/N");
 		Scanner retryScan = new Scanner(System.in); //Creating a place for the input to be stores
 		String retryInput = retryScan.nextLine(); //Getting the input and storing it
-	    retryVerification(retryInput);
+	    retryVerification(retryInput); //Call 
 	}
    
 	public static void retryVerification(String retryInput) {
